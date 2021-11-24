1432c658a0c34e62609cf868dbc19aed4d5f3936
Piper Wallingford <piper.wallingford@gmail.com>
2021-11-23 18:01:13 -0800

    Add files via upload

---
+++ Scripts/Whelk Map.R
@@ -0,0 +1,135 @@
+###########################################################
+#  Project: map of whelk species                          #
+#  Created by Piper Wallingford                           #
+#  Created on 7 July 2020                                 #
+###########################################################
+
+# clear workspace
+rm(list=ls())
+
+# Load Packages
+library("maps")
+library("mapdata")
+library("raster")
+library("plotrix")
+library("marmap")
+library(sf)   
+library(spData)        
+library(spDataLarge) 
+library("tmaptools")
+library("tigris")
+library("ggplot2")
+library("scatterpie")
+library("ggtree")
+library("png")
+
+
+# Whelk Data
+whelks <- read.csv("Data/Whelks.csv")
+whelks$Density <- round(whelks$Density, 2)
+
+
+png(paste('Plots/LC.png'), width = 800, height = 800)
+ggplot(whelks[whelks$Site == "LC",], aes(x = "", y = Density, fill = Species)) +
+  geom_col() +
+  scale_fill_manual(values = c("aquamarine4","mediumpurple3","peachpuff","lemonchiffon")) +
+  #geom_text(aes(label = Density), position = position_stack(vjust = 0.5), size = 7) +
+  coord_polar(theta = "y") +
+  theme_void() +
+  theme(legend.position = "none")
+dev.off()
+
+LC <- readPNG('Plots/LC.png')
+
+
+png(paste('Plots/SH.png'), width = 800, height = 800)
+ggplot(whelks[whelks$Site == "SH",], aes(x = "", y = Density, fill = Species)) +
+  geom_col() +
+  scale_fill_manual(values = c("aquamarine4","mediumpurple3","peachpuff","lemonchiffon")) +
+  #geom_text(aes(label = Density), position = position_stack(vjust = 0.5), size = 7) +
+  coord_polar(theta = "y") +
+  theme_void() +
+  theme(legend.position = "none")
+dev.off()
+
+SH <- readPNG('Plots/SH.png')
+
+
+png(paste('Plots/TI.png'), width = 800, height = 800)
+ggplot(whelks[whelks$Site == "TI",], aes(x = "", y = Density, fill = Species)) +
+  geom_col() +
+  scale_fill_manual(values = c("aquamarine4","mediumpurple3","peachpuff","lemonchiffon")) +
+  #geom_text(aes(label = Density), position = position_stack(vjust = 0.5), size = 7) +
+  coord_polar(theta = "y") +
+  theme_void() +
+  theme(legend.position = "none")
+dev.off()
+
+TI <- readPNG('Plots/TI.png')
+
+
+png(paste('Plots/CR.png'), width = 800, height = 800)
+ggplot(whelks[whelks$Site == "CR",], aes(x = "", y = Density, fill = Species)) +
+  geom_col() +
+  scale_fill_manual(values = c("aquamarine4","mediumpurple3","peachpuff","lemonchiffon")) +
+  #geom_text(aes(label = Density), position = position_stack(vjust = 0.5), size = 7) +
+  coord_polar(theta = "y") +
+  theme_void() +
+  theme(legend.position = "none")
+dev.off()
+
+CR <- readPNG('Plots/CR.png')
+
+
+png(paste('Plots/SC.png'), width = 400, height = 400)
+ggplot(whelks[whelks$Site == "SC",], aes(x = "", y = Density, fill = Species)) +
+  geom_col() +
+  scale_fill_manual(values = c("aquamarine4","mediumpurple3","peachpuff","lemonchiffon")) +
+  #geom_text(aes(label = Density), position = position_stack(vjust = 0.5), size = 7) +
+  coord_polar(theta = "y") +
+  theme_void() +
+  theme(panel.background = element_rect(fill = "transparent"), # bg of the panel
+    plot.background = element_rect(fill = "transparent", color = NA), # bg of the plot
+    panel.grid.major = element_blank(), # get rid of major grid
+    panel.grid.minor = element_blank(), # get rid of minor grid
+    legend.background = element_rect(fill = "transparent"), # get rid of legend bg
+    legend.box.background = element_rect(fill = "transparent") # get rid of legend panel bg
+  ) +
+  theme(legend.position = "none")
+dev.off()
+
+SC <- readPNG('Plots/SC.png')
+
+
+
+# California Counties
+CA <- counties("California", cb = TRUE)
+counties <- CA[CA$NAME == "Los Angeles" | CA$NAME == "San Bernardino" | CA$NAME == "San Diego" | CA$NAME == "Orange" | CA$NAME == "Ventura" | CA$NAME == "Riverside" | CA$NAME == "Imperial" | CA$NAME == "Kern" | CA$NAME == "San Luis Obispo" | CA$NAME == "Santa Barbara",]
+
+# Mexico States
+Mex <- world[world$name_long == "Mexico",]
+
+
+png(paste('Plots/Map.png'), width = 2400, height = 2400)
+ggplot() +
+  geom_sf(data = counties, color="black", fill = "darkolivegreen3", size=0.25) +
+  geom_sf(data = Mex, color = "black", fill = "gray", size=0.25) +
+  annotate(geom = "text", x = -117, y = 32.8, label = "San Diego", color = "black", size = 25) +
+  annotate(geom = "text", x = -118.25, y = 34, label = "Los Angeles", color = "black", size = 25) +
+  annotate(geom = "text", x = -117.75, y = 33.65, label = "Irvine", color = "black", size = 25) +
+  annotate(geom = "text", x = -118.35, y = 33.6, label = "Corona", color = "navy", size = 20) +
+  annotate(geom = "text", x = -118.2, y = 33.5, label = "Shaw's", color = "navy", size = 20) +
+  annotate(geom = "text", x = -118.115, y = 33.38, label = "Treasure Island", color = "navy", size = 20) +
+  annotate(geom = "text", x = -117.625, y = 33, label = "Cardiff", color = "navy", size = 20) +
+  annotate(geom = "text", x = -117.625, y = 32.85, label = "Scripps", color = "navy", size = 20) +
+  #annotation_raster(SC, ymin = 33.599, ymax= 36.08,xmin = -117.91, xmax = -118) +
+  xlim(-118.75,-116.7) +
+  ylim(32.45,34) + 
+  xlab("Longitude") +
+  ylab("Latitude") +
+  theme_bw() + 
+  theme(axis.text = element_text(size = 35)) +
+  theme(axis.title = element_text(size = 50)) +
+  guides(fill = "none") +
+  theme(panel.background = element_rect(fill = "aliceblue")) 
+dev.off()
---
+++ Scripts/Whelk Tide Height Distributions.R
@@ -0,0 +1,162 @@
+###########################################################
+#  Whelk Tide Height Distributions                        #
+#  Created by Piper Wallingford                           #
+#  Created on 20 September 2018                           #
+###########################################################
+
+
+# clear workspace
+rm(list=ls())
+
+
+# Load Packages
+library("tidyverse")
+library("ggplot2")
+library("ggeffects")
+library("lme4")
+
+
+
+#### Read in data ####
+Data <- read.csv("Data/SurveyData.csv") 
+TH.MPA <- read.csv("Data/TH Distributions PA.csv")
+
+
+# Data Summary
+
+# calculate densities
+Data$Mdens = Data$M.lugubris/Data$Area
+Data$Adens = Data$A.spirata/Data$Area
+Data$Ndens = Data$N.emarginata/Data$Area
+Data$Other.dens = (Data$Ceres + Data$Conus + Data$Max + Data$Olive)/Data$Area
+Data$Native.dens = (Data$A.spirata + Data$N.emarginata)/Data$Area
+
+# Assign Regions
+Data$Region <- NULL
+Data$Region <- ifelse(Data$Site == "HP" | Data$Site == "TI", "Expanding", "Established")
+Data$Region <- ifelse(Data$Site == "CC" | Data$Site == "LC" | Data$Site == "SH", "Absent", Data$Region)
+
+
+# Split by Mex  presence
+Data$M.PA <- NULL
+Data$M.PA = ifelse(Data$M.lugubris!=0, 1, 0)
+Data$N.PA <- NULL
+Data$N.PA = ifelse(Data$Native!=0, 1, 0)
+
+
+# Tidy 
+Data <- Data %>% 
+  mutate(Season=as.factor(Season), Site=as.factor(Site), Transect=as.factor(Transect), Region = as.factor(Region), TH=as.numeric(TH))
+Data$Region <- ordered(Data$Region, levels = c("Absent", "Expanding", "Established"))
+
+
+# Summarize
+Trnsct.Data <- Data %>% #average across transects
+  group_by(Season, Site, TH, Region) %>%
+  summarize(mean.native= mean(Native.dens),
+            se.nat = sd(Native.dens)/sqrt(n()),
+            mean.mex = mean(Mdens),
+            se.mex = sd(Mdens)/sqrt(n())) %>%
+  mutate(N.PA=ifelse(mean.native>0,1,0)) %>%
+  mutate(M.PA=ifelse(mean.mex>0,1,0))
+
+
+#### Data Analysis ####
+
+# Presence/Absence Hurdle Models
+binomial2 <- glm(N.PA ~ M.PA * TH, data = Trnsct.Data, family = binomial(link = logit))
+gamma2 <- glm(mean.native ~ M.PA * TH, data = Trnsct.Data[Trnsct.Data$N.PA == 1,], family = Gamma(link = log))
+
+
+
+#### Plot - Presence of Native Whelks in the Presence of Non-Native Whelks ####
+png("Plots/THdistributions.png", width = 2400, height = 1600, res = 300)
+ggplot(data=TH.MPA, aes(x = TH, y = Density, fill = Species, color = Species)) +
+  facet_grid(~MPA) +
+  geom_point(aes(y = Density, color = Species)) +
+  geom_errorbar(aes(ymin=Density-se, ymax=Density+se, color = Species), width = 0.05) +
+  geom_ribbon(aes(ymin = 0, ymax = Density, color = Species), alpha = 0.6) +
+  scale_color_manual(values = c("mediumpurple3","aquamarine4")) + 
+  scale_fill_manual(values = c("mediumpurple3","aquamarine4")) +
+  xlab("Tidal Elevation (m)") +
+  ylab(expression(paste('Density (per m'^{2},')'))) +
+  scale_y_continuous(expand = c(0.0,0.05)) +
+  scale_x_continuous(expand = c(0.001,0.001)) +
+  theme(strip.background = element_blank(),
+        strip.text.y = element_text(size = 12),
+        strip.text.x = element_text(size = 12),
+        panel.border = element_rect(colour = "gray", fill=NA, size=1),
+        panel.background = element_blank(), 
+        panel.grid = element_blank(), 
+        panel.spacing.x = unit(1,"cm"),
+        axis.text.y=element_text(size = 14),
+        axis.text.x=element_text(size=14),
+        axis.title=element_text(size=16)) + 
+  theme(legend.position = c(0.05, 0.95), 
+        legend.key = element_rect(colour = 'white', fill = 'white', size = 0.5),
+        legend.justification = c("left", "top"), 
+        legend.box.just = "left",
+        legend.title.align=0.35,
+        legend.title=element_text(size=12),
+        legend.text=element_text(size=12, face = "italic"))
+dev.off()
+
+
+
+#### Plot - Presence of Native Whelks in the Presence of Non-Native Whelks ####
+png("Plots/Figure3c.png", width = 2400, height = 1600, res = 300)
+plot(ggpredict(binomial2, c("TH","M.PA"), interactive = TRUE, se = TRUE, facet = TRUE)) + 
+  geom_smooth(method = "glm", 
+              method.args = list(family = "binomial"), 
+              se = FALSE) +
+  scale_color_manual(name = expression(paste(italic("Mexacanthina "), "Presence")), labels = c("Absent", "Present"), values = c("aquamarine4","mediumpurple4")) +
+  scale_fill_manual(values = c("aquamarine4","mediumpurple4")) +
+  labs(x = "Tidal Elevation (m)", y = "Probability of Native Whelk Presence") +
+  theme(strip.background = element_blank(),
+        strip.text.y = element_text(size = 12),
+        panel.border = element_rect(colour = "gray", fill=NA, size=1),
+        panel.background = element_blank(), 
+        panel.grid = element_blank(), 
+        panel.spacing.x = unit(0,"line"),
+        axis.text.y=element_text(size = 14),
+        axis.text.x=element_text(size=14),
+        axis.title=element_text(size=16)) +
+  theme(axis.line.x = element_line(color="gray", size = 0.25),
+        legend.position = c(0.95,0.95), 
+        legend.justification = c("right", "top"), 
+        legend.box.just = "right",
+        legend.title.align=0.35,
+        legend.title=element_text(size=14),
+        legend.key = element_rect(fill = NA),
+        legend.text=element_text(size=12)) 
+dev.off()
+
+
+
+#### Plot - Density of Native Whelks in the Presence of Non-Native Whelks ####
+png("Plots/Density.png", width = 2400, height = 1600, res = 300)
+plot(ggpredict(gamma2, terms = c("TH", "M.PA"), interactive = TRUE, se = TRUE, facet = TRUE)) + 
+  geom_smooth(method = "glm", 
+              method.args = list(family = "binomial"), 
+              se = FALSE) +
+  scale_color_manual(name = expression(paste(italic("Mexacanthina "), "Presence")), labels = c("Absent", "Present"), values = c("aquamarine4","mediumpurple4")) +
+  scale_fill_manual(values = c("aquamarine4","mediumpurple4")) +
+  labs(x = "Tidal Elevation (m)", y = "Density of Native Whelks") +
+  theme(strip.background = element_blank(),
+        strip.text.y = element_text(size = 12),
+        panel.border = element_rect(colour = "gray", fill=NA, size=1),
+        panel.background = element_blank(), 
+        panel.grid = element_blank(), 
+        panel.spacing.x = unit(0,"line"),
+        axis.text.y=element_text(size = 14),
+        axis.text.x=element_text(size=14),
+        axis.title=element_text(size=16)) +
+  theme(axis.line.x = element_line(color="gray", size = 0.25),
+        legend.position = c(0.95,0.95), 
+        legend.justification = c("right", "top"), 
+        legend.box.just = "right",
+        legend.title.align=0.35,
+        legend.title=element_text(size=14),
+        legend.key = element_rect(fill = NA),
+        legend.text=element_text(size=12)) 
+dev.off()
